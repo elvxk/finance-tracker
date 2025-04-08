bc22315bf0843f7a825876ce5b5bbcc920d54f4c
Pradipta R. Sandri <pradiptaramadan9c@gmail.com>
2025-04-08 20:30:29 +0700

    Feat/favicon (#1)
    * feat(favicon): add new favicon
    
    * feat(ui): add shadcn ui
    
    * feat(theme): add next-theme
    
    * feat(components): add button from ui
    
    * feat(theme): add dark mode

---
+++ src/app/layout.jsx
@@ -0,0 +1,44 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Finance Tracker",
+  description:
+    " A user-friendly web app to track your income and expenses, helping you manage your finances and budget with ease.",
+  icons: {
+    icon: ["/favicon-16x16.png", "/favicon-32x32.png", "/favicon.ico"],
+    apple: "/apple-touch-icon.png",
+    android: ["/android-chrome-192x192.png", "/android-chrome-512x512.png"],
+  },
+  manifest: "/site.webmanifest",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
