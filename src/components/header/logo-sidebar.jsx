c5bd15494fcbb751274628f03909b954b88707d7
elvxk <pradiptaramadan9c@gmail.com>
2025-04-12 11:08:03 +0700

    feat(auth): add authentication with sso

--- src/components/header/logo-sidebar.jsx
+++ src/components/header/logo-sidebar.jsx
@@ -1,24 +1,28 @@
-import { BadgeSwissFranc } from "lucide-react";
+import { BadgeSwissFranc, PanelLeftClose } from "lucide-react";
 import { DropdownMenu, DropdownMenuTrigger } from "../ui/dropdown-menu";
-import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "../ui/sidebar";
 
 const LogoSidebar = () => {
+  const { toggleSidebar } = useSidebar();
   return (
     <SidebarMenu>
       <SidebarMenuItem>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
+            <SidebarMenuButton size="lg" onClick={toggleSidebar}>
               <div className="flex size-8 items-center justify-center aspect-square bg-emerald-0">
                 <BadgeSwissFranc />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">Finance Tracker</span>
-                <span className="truncate text-xs">v.1.0.0</span>
+                <span className="truncate text-xs opacity-30">v.1.0.0</span>
               </div>
+              <PanelLeftClose className="ml-auto" />
             </SidebarMenuButton>
           </DropdownMenuTrigger>
         </DropdownMenu>
--- src/components/header/top-sidebar.jsx
+++ src/components/header/top-sidebar.jsx
@@ -7,7 +7,6 @@ const TopSidebar = ({ user }) => {
   return (
     <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-12 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear top-0 sticky z-10 bg-background">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-        {/* {user && <SidebarTrigger className="-ml-1" />} */}
         <SidebarTrigger className="-ml-1" />
         <Separator
           orientation="vertical"
