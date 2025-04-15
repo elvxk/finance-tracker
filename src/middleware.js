a98080a9def8bea67d70314d05748a29ce0fb493
elvxk <pradiptaramadan9c@gmail.com>
2025-04-15 19:39:55 +0700

    fix(auth): fix login SSO stuck

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/login(.*)", "/api(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/login(.*)",
+  "/api(.*)",
+  "/register(.*)",
+]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
